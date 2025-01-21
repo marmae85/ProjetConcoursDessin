0b2ae136cbd7e9d40aa74781799c38c5138648bb
Marius <marius.humeau@gmail.com>
2025-01-21 11:20:01 +0100

    Écriture des requêtes obligatoires 3 et 4

--- SQL/requetesObligatoires.sql
+++ SQL/requetesObligatoires.sql
@@ -14,5 +14,25 @@ and d.numDessin=eval.numDessin
 and cc.numConcours=d.numConcours
 and d.numCompetiteur=cp.numCompetiteur
 and cp.numCompetiteur=u.numUtilisateur
-ORDER BY d.numDessin ASC;
+ORDER BY eval.note ASC;
 
+SELECT cc.numConcours, YEAR(cc.dateDebut), cc.description, u.nom, d.numDessin, d.commentaire, eval.note, eval.commentaire, evaluat.nom
+FROM Dessin d, Concours cc, Competiteur c, Utilisateur u, Evaluation eval, Evaluateur e, Utilisateur evaluat
+WHERE d.numConcours=cc.numConcours
+and d.numCompetiteur=c.numCompetiteur and c.numCompetiteur=u.numUtilisateur
+and eval.numDessin=d.numDessin
+and eval.numEvaluateur=e.numEvaluateur
+and evaluat.numUtilisateur=e.numEvaluateur
+ORDER BY d.numDessin;
+
+SELECT u.nom, u.prenom, TIMESTAMPDIFF(YEAR, u.dateNaissance, CURDATE()) AS age
+FROM Competiteur c, Utilisateur u, Competiteur_Participe cp, Concours conc
+WHERE c.numCompetiteur = u.numUtilisateur
+AND c.numCompetiteur = cp.numCompetiteur
+AND cp.numConcours = conc.numConcours
+AND (SELECT COUNT(*)
+     FROM Concours) = (SELECT COUNT(DISTINCT cp2.numConcours)
+                       FROM Competiteur_Participe cp2
+                       WHERE cp2.numCompetiteur = c.numCompetiteur)
+GROUP BY c.numCompetiteur
+ORDER BY age ASC;
